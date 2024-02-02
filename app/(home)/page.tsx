b189c8b397dd4f129c7fd286429ae389b5f6e609
Worley Sacramento <78088577+WorleySacramento@users.noreply.github.com>
2024-02-02 11:42:17 -0300

    feat
     add bem vindo

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
+import {format} from 'date-fns'
 import Header from "../_components/header";
+import { ptBR } from 'date-fns/locale/pt-BR';
+
 
 export default function Home() {
   return (
     <>
  <Header/>
+ <div className=' px-5 pt-5'>
+ <h2 className='text-xl font-bold font-sans '>Olá user</h2>
+ <p className='capitalize'>{format(new Date(), "EEEE',' dd 'de' MMM 'de' yyyy",{
+  locale: ptBR,
+ })}
+ </p>
+ </div>
     </>
   );
 }
