47bcd32c1b20e9e5be92d37b00481b81e419c272
Worley Sacramento <78088577+WorleySacramento@users.noreply.github.com>
2024-02-02 11:56:10 -0300

    feat
    add search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import {format} from 'date-fns'
 import Header from "../_components/header";
 import { ptBR } from 'date-fns/locale/pt-BR';
+import Search from './_components/search';
 
 
 export default function Home() {
@@ -13,6 +14,9 @@ export default function Home() {
   locale: ptBR,
  })}
  </p>
+ </div>
+ <div className="px-5 mt-6">
+ <Search/>
  </div>
     </>
   );
